dc57c93ffb78f609e63e7be8e43b09eb807dca76
Dwayne Campogan <dcampogan@gmail.com>
2020-02-04 22:04:28 -0800

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import Home from './HomeComponent';
 import Directory from "./directoryComponent";
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import { View, Platform } from 'react-native';
 import CampsiteInfo from "./CampsiteInfoComponent";
 
@@ -23,6 +24,32 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator (
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator }
+  },
+  {
+    drawerBackgroundColor: '#cec8ff'
+  }
+);
 
 class Main extends Component {
   render() {
@@ -32,7 +59,7 @@ class Main extends Component {
           flex: 1,
           paddingTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight
         }}>
-        <DirectoryNavigator />
+        <MainNavigator />
       </View>
     );
   }
