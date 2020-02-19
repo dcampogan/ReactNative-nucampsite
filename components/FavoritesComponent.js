6cd6fde1cd50f9f962f8884f77ded06b0d0a5abb
Dwayne Campogan <dcampogan@gmail.com>
2020-02-18 20:01:29 -0800

    Swipeout and Delete Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,6 +4,8 @@ import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import Swipeout from "react-native-swipeout";
+import { deleteFavorite } from "../redux/ActionCreators";
 
 const mapStateToProps = state => {
   return {
@@ -12,25 +14,42 @@ const mapStateToProps = state => {
   };
 };
 
+const mapDispatchToProps = {
+  deleteFavorite: campsiteId => deleteFavorite(campsiteId)
+};
+
 class Favorites extends Component {
   static navigationOptions = {
     title: "My Favorites"
   };
+
   render() {
     const { navigate } = this.props.navigation;
     const renderFavoriteItem = ({ item }) => {
+      const rightButton = [
+        {
+          text: "Delete",
+          type: "delete",
+          onPress: () => this.props.deleteFavorite(item.id)
+        }
+      ];
+      
       return (
-        <ListItem
-          title={item.name}
-          subtitle={item.description}
-          leftAvatar={{ source: { uri: baseUrl + item.image } }}
-          onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
-        />
+        <Swipeout right={rightButton} autoClose={true}>
+          <ListItem
+            title={item.name}
+            subtitle={item.description}
+            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+            onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+          />
+        </Swipeout>
       );
     };
+
     if (this.props.campsites.isLoading) {
       return <Loading />;
     }
+
     if (this.props.campsites.errMess) {
       return (
         <View>
@@ -38,6 +57,7 @@ class Favorites extends Component {
         </View>
       );
     }
+
     return (
       <FlatList
         data={this.props.campsites.campsites.filter(campsite =>
@@ -49,4 +69,4 @@ class Favorites extends Component {
     );
   }
 }
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
